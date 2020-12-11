557d67d5f0c3c6a0964333a39eee4394553ec423
Mr.Tang <geektcp@163.com>
2020-12-11 20:34:46 +0800

    three.js 3d 前端demo

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorldS msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorldS from './components/HelloWorldS.vue'
+
 export default {
   name: 'App',
   components: {
-      HelloWorld
+      HelloWorldS
   }
 }
 </script>
--- src/components/HelloWorldS.vue
+++ src/components/HelloWorldS.vue
@@ -41,43 +41,87 @@
         props: {
             msg: String
         },
-        mounted() {
-            var box_geometry = new THREE.BoxGeometry();
-            var sphere_geometry = new THREE.SphereGeometry(0.5, 32, 32);
-            var cylinder_geometry = new THREE.CylinderGeometry(0.1, 0.1, 0.5);
-            var material = new THREE.MeshLambertMaterial({color: new THREE.Color(0.9, 0.55, 0.4)})
-            var box = new THREE.Mesh(box_geometry, material);
-            var sphere = new THREE.Mesh(sphere_geometry, material);
-            sphere.position.y += 1;
-            var cylinder = new THREE.Mesh(cylinder_geometry, material);
-            cylinder.position.y += 1.75;
-            scene.add(box);
-            scene.add(sphere);
-            scene.add(cylinder);
-            box.scale.multiplyScalar(0.5);
-            sphere.scale.multiplyScalar(0.5);
-            cylinder.scale.multiplyScalar(0.5);
-            var pile = new THREE.Object3D();
-            pile.scale.multiplyScalar(0.5);
-            pile.add(box);
-            pile.add(sphere);
-            pile.add(cylinder);
-            scene.add(pile);
+        data() {
+            return {
+                pile: null,
+                renderer: null,
+                scene: null,
+                camera: null
+            }
+        },
+        methods: {
+            init: function() {
+                this.scene = new THREE.Scene();
+                this.camera = new THREE.PerspectiveCamera(
+                    75,
+                    window.innerWidth / window.innerHeight,
+                    0.1,
+                    1000
+                );
+                this.renderer = new THREE.WebGLRenderer();
+                this.renderer.setSize(window.innerWidth, window.innerHeight);
+                document.body.appendChild(this.renderer.domElement);
 
-            var sphere_matrix = new THREE.Matrix4().makeTranslation(0.0, 1.0, 0.0);
-            sphere.applyMatrix(sphere_matrix);
-            var cylinder_matrix = sphere_matrix.clone(); cylinder_matrix.multiply(new THREE.Matrix4().makeTranslation(0.0, 0.75, 0.0));
-            cylinder.applyMatrix(cylinder_matrix);
-            box.scale.multiplyScalar(0.5);
-            sphere.scale.multiplyScalar(0.5);
-            cylinder.scale.multiplyScalar(0.5);
-            var pile = new THREE.Object3D();
-            pile.scale.multiplyScalar(0.5);
-            pile.add(box);
-            pile.add(sphere);
-            pile.add(cylinder);
-            scene.add(pile);
-        }
+                const box_geometry = new THREE.BoxGeometry();
+                const sphere_geometry = new THREE.SphereGeometry(0.5, 32, 32);
+                const cylinder_geometry = new THREE.CylinderGeometry(0.1, 0.1, 0.5);
+                // const material = new THREE.MeshLambertMaterial({color: new THREE.Color(0.9, 0.55, 0.4)})
+                // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+                const material = new THREE.MeshBasicMaterial({ color: new THREE.Color(0.9, 0.55, 0.4) });
+                this.box = new THREE.Mesh(box_geometry, material);
+                this.sphere = new THREE.Mesh(sphere_geometry, material);
+                this.sphere.position.y += 1;
+                const cylinder = new THREE.Mesh(cylinder_geometry, material);
+                cylinder.position.y += 1.75;
+
+                this.scene.add(this.box);
+                this.scene.add(this.sphere);
+                this.scene.add(cylinder);
+                this.box.scale.multiplyScalar(0.5);
+                // sphere.scale.multiplyScalar(0.5);
+                // cylinder.scale.multiplyScalar(0.5);
+                // this.pile = new THREE.Object3D();
+                // this.pile.scale.multiplyScalar(0.5);
+                // this.pile.add(this.box);
+                // this.pile.add(sphere);
+                // this.pile.add(cylinder);
+                // this.scene.add(this.pile);
+
+                // const geometry = new THREE.BoxGeometry(1, 1, 1)
+                // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+                // this.cube = new THREE.Mesh(geometry, material)
+                // this.scene.add(this.cube)
+
+                this.camera.position.z = 5
+
+                const animate = function() {}
+            },
+            animate: function() {
+                requestAnimationFrame(this.animate)
+
+                this.box.rotation.x += 0.01
+                this.box.rotation.y += 0.01
+                this.box.rotation.z += 0.3
+
+                this.sphere.rotation.x += 0.01
+                this.sphere.rotation.y += 0.01
+                this.sphere.rotation.z += 0.3
+
+                this.sphere.x += 0.01
+                this.sphere.y += 0.01
+                this.sphere.z += 0.3
+
+                this.camera.z += 0.3
+
+                this.renderer.render(this.scene, this.camera)
+            }
+        },
+
+        mounted() {
+            this.init()
+            this.animate()
+        },
+
     }
 </script>
 
