8771dbe1716ea3e75e7b8b7fb9d84f615f7be448
geektcp <geektcp@163.com>
2022-01-01 22:01:45 +0800

    最佳vue的写法。 并且把页面放到view路径下

--- src/main.js
+++ src/main.js
@@ -1,28 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router'
-Vue.use(VueRouter)
+import router from './router'
 
 Vue.config.productionTip = false
-
-// import router from './router'
-
-// const routes = [
-//   { path: '/a', component: () => import('@/components/HelloWorldA'), },
-//   { path: '/b', component: () => import('@/components/HelloWorldB'), },
-// ]
-
-import HelloWorldA from '@/components/HelloWorldA'
-import HelloWorldB from "@/components/HelloWorldB";
-const routes = [
-    { path: '/a',  component: HelloWorldA  },
-    { path: '/b',  component: HelloWorldB }
-
-]
-
-const router = new VueRouter({
-  routes // (缩写) 相当于 routes: routes
-})
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -2,24 +2,19 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
-import HelloWorldA from '../components/HelloWorldA'
-import HelloWorldB from "../components/HelloWorldB";
-// const routes = [
-//     { path: '/a',  component: HelloWorldA  },
-//     { path: '/b',  component: HelloWorldB }
-//
-// ]
+import HelloWorldA from '@/view/HelloWorldA'
+import HelloWorldB from "@/view/HelloWorldB";
 
 const routes = [
-    { path: '/a',  component: () => import('@/components/HelloWorldA')  },
-    { path: '/b',  component: () => import('@/components/HelloWorldB')  }
+    { path: '/',   component: HelloWorldA },
+    { path: '/a',  component: HelloWorldA  },
+    { path: '/b',  component: HelloWorldB }
 
 ]
 
 
-
 const router = new VueRouter({
-    routes // (缩写) 相当于 routes: routes
+    routes: routes
 })
 
 export default router
