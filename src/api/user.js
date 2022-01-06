a5791572c1d2e5115c8134ef3638256333f034ae
geektcp <geektcp@163.com>
2022-01-06 22:48:20 +0800

    test data

--- src/api/user.js
+++ src/api/user.js
@@ -22,3 +22,18 @@ export function logout() {
     method: 'post'
   })
 }
+
+// export function login(data) {
+//     let ret =  {code: 20000, data: {token: 'admin-token'}}
+//     ret
+// }
+//
+// export function getInfo(token) {
+//     let ret =   {code:20000,data:{roles:["admin"],introduction:"I am a super administrator",avatar:"https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif", name:"Super Admin"}}
+//     ret
+// }
+//
+// export function logout() {
+//     let ret =  {code: 20000, data: 'success'}
+//     ret
+// }
