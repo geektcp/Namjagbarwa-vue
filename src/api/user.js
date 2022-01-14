0859af249cc3084bdd2f635cd0a15140f9c20318
Mr.Tang <geektcp@163.com>
2022-01-14 17:33:28 +0800

    模拟登录登出的数据

--- src/api/user.js
+++ src/api/user.js
@@ -10,7 +10,7 @@ import request from '@/utils/request'
 
 // export function getInfo(token) {
 //   return request({
-//     url: '/vue-element-admin/user/info',
+//     url: '/vue-element-admin/user/info.json',
 //     method: 'get',
 //     params: { token }
 //   })
@@ -23,41 +23,58 @@ import request from '@/utils/request'
 //   })
 // }
 
-export function login(data) {
-  console.log(data)
-  return new Promise(function(resolve){
-      resolve({code: 20000, data: {token: 'admin-token'}});
-    })
-}
-
+/////////////////////////////////////////////////////////////////////
 // export function login(data) {
-//   return request({
-//     url: '/api/login.json',
-//     method: 'get',
-//     data
+//   console.log(data)
+//   return new Promise(function(resolve){
+//       resolve({code: 20000, data: {token: 'admin-token'}});
+//     })
+// }
+
+// export function getInfo(token) {
+//   console.log(token)
+//   return new Promise(function(resolve) {
+//     setTimeout(function () {
+//       resolve({
+//         code: 20000,
+//         data: {
+//           roles: ["admin"],
+//           introduction: "I am a super administrator",
+//           avatar: "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif",
+//           name: "Super Admin"
+//         }
+//       });
+//     }, 1000)
 //   })
 // }
 
-export function getInfo(token) {
-  console.log(token)
-  return new Promise(function(resolve) {
-    setTimeout(function () {
-      resolve({
-        code: 20000,
-        data: {
-          roles: ["admin"],
-          introduction: "I am a super administrator",
-          avatar: "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif",
-          name: "Super Admin"
-        }
-      });
-    }, 1000)
+
+// export function logout() {
+//   return new Promise(function(resolve){
+//     resolve({code: 20000, data: 'success'});
+//   })
+// }
+
+
+//////////////////////////////////////////////////////
+export function login(data) {
+  return request({
+    url: '/login.json',
+    method: 'get',
+    data
   })
 }
 
+export function getInfo() {
+  return request({
+    url: '/user/info.json',
+    method: 'get',
+  })
+}
 
 export function logout() {
-  return new Promise(function(resolve){
-    resolve({code: 20000, data: 'success'});
+  return request({
+    url: '/logout.json',
+    method: 'get',
   })
 }
